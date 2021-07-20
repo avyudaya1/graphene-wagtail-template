7048cb1501a03a6889d89a5a2655418099579e33
avyudaya1 <avyudaya1@gmail.com>
2021-07-20 19:25:44 +0545

    init blog from documentation

---
+++ backend/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-8%d7r!%r^%puw+-+t%rp9gdysl3@sclbz5la6zy$0ls1*=eb0y'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
